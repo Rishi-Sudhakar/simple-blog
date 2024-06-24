feb2cf41f2a6bae3b0cceaafc05c575cf62d36f7
Rishi <rishisudhakar123@gmail.com>
2024-06-25 01:50:10 +0530

    Fix #2

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ktrzaapgnvrtwghyaxwr.supabase.co'
+const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
